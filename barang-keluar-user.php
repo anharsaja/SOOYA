e35ae5b2003a80ca3f5ff6601fb6908f7c0170a0
anharsaja <anharmuk12@gmail.com>
2023-06-03 23:58:13 +0700

    untuk user

---
+++ barang-keluar-user.php
@@ -0,0 +1,204 @@
+<?php
+//import koneksi
+require 'function.php';
+// require 'cek.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Barang Keluar</title>
+    <link rel="icon" type="png" href="assets/img/trade.png">
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="keluar.php">G U D A N G</a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+
+
+                        <div class="sb-sidenav-menu-heading">
+                            <h5>PRODUCT</h5>
+                        </div>
+
+                        <a class="nav-link" href="produk-user-premium.php">
+                            <div class="sb-nav-link-icon"><i class="fa-sharp fa-solid fa-warehouse fa-bounce"></i></div>
+                            Daftar Produk
+                        </a>
+
+                        <a class="nav-link" href="pemesanan-user.php">
+                            <div class="sb-nav-link-icon"><i class="fa-sharp fa-solid fa-warehouse fa-bounce"></i></div>
+                            Daftar Pemesanan
+                        </a>
+
+                        <div class="sb-sidenav-menu-heading">
+                            <h5>BAHAN BAKU</h5>
+                        </div>
+
+                        <a class="nav-link" href="barang-masuk-user.php">
+                            <div class="sb-nav-link-icon"><i class="fa-sharp fa-solid fa-warehouse fa-bounce"></i></div>
+                            Bahan Masuk
+                        </a>
+
+                        <a class="nav-link" href="barang-keluar-user.php">
+                            <div class="sb-nav-link-icon"></div>
+                            Bahan Keluar
+                        </a>
+
+
+                    </div>
+                    <div class="nav">
+                        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                        <div class="card-header">
+                            <!-- Button to Open the Modal -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                Ubah Data Akun
+                            </button>
+                        </div>
+                        <a class="nav-link" href="logout.php">
+                            <div class="sb-nav-link-icon"></i></div>
+                            Logout
+                        </a>
+
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4">BAHAN KELUAR</h1>
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Tanggal</th>
+                                            <th>Plat Nomor</th>
+                                            <th>Kode Barang</th>
+                                            <th>Nama Barang</th>
+                                            <th>Jumlah</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $ambilsemuadatastock = mysqli_query($conn, "select * from keluar k, stock s where s.idbarang = k.idbarang");
+                                        while ($data = mysqli_fetch_array($ambilsemuadatastock)) {
+                                            $idb = $data['idbarang'];
+                                            $tanggal = $data['tanggal'];
+                                            $penerima = $data['penerima'];
+                                            $namabarang = $data['namabarang'];
+                                            $qty = $data['qty'];
+                                        ?>
+
+                                            <tr>
+                                                <td><?= $tanggal; ?></td>
+                                                <td><?= $penerima; ?></td>
+                                                <td><?= $idb; ?></td>
+                                                <td><?= $namabarang; ?></td>
+                                                <td><?= $qty; ?></td>
+                                            </tr>
+
+                                            
+
+                                        <?php
+                                        };
+                                        ?>
+
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+
+
+            <!-- Bisa diisi dengan median sosial pengguna atau medsis perusahaan, kalau 
+                website hanya untuk admin berarti tidak usah dikasih footer atau copyright-->
+
+            <!-- <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer> -->
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/datatables-demo.js"></script>
+</body>
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah Barang Keluar</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <form method="post">
+                <div class="modal-body">
+                    <select name="barangnya" class="form-control">
+                        <?php
+                        $ambilsemuadatanya = mysqli_query($conn, "select * from stock");
+                        while ($fetcharray = mysqli_fetch_array($ambilsemuadatanya)) {
+                            $idbarangnya = $fetcharray['idbarang'];
+
+                        ?>
+
+                            <option value="<?= $idbarangnya; ?>"><?= $idbarangnya; ?></option>
+
+                        <?php
+                        }
+                        ?>
+                    </select>
+                    <br>
+
+                    <input type="text" name="penerima" placeholder="Penerima (Plat Nomor)" class="form-control" required>
+                    <br>
+                    <input type="number" name="qty" placeholder="Quantity" class="form-control" required>
+                    <br>
+                    <button type="submit" class="btn btn-primary" name="addbarangkeluar">Submit</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+</html>
