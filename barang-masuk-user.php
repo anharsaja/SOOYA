a0b77130fba83a8927282fc23b2ca3e6a60ffe02
anharsaja <anharmuk12@gmail.com>
2023-06-04 03:26:39 +0700

    monitoring m user

---
+++ barang-masuk-user.php
@@ -0,0 +1,172 @@
+<?php
+//import koneksi
+require 'function.php';
+// require 'cek.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>User Account Data</title>
+    <link rel="icon" type="png" href="assets/img/warehouse.png">
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="akun-admin.php">
+            " S O O Y A "
+        </a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+
+
+                        <div class="sb-sidenav-menu-heading">
+                            <h5>PRODUCT</h5>
+                        </div>
+
+                        <a class="nav-link" href="produk-user-premium.php">
+                            <div class="sb-nav-link-icon"><i class="fa-sharp fa-solid fa-warehouse fa-bounce"></i></div>
+                            Daftar Produk
+                        </a>
+
+                        <a class="nav-link" href="pemesanan-user.php">
+                            <div class="sb-nav-link-icon"><i class="fa-sharp fa-solid fa-warehouse fa-bounce"></i></div>
+                            Daftar Pemesanan
+                        </a>
+
+                        <div class="sb-sidenav-menu-heading">
+                            <h5>BAHAN BAKU</h5>
+                        </div>
+
+                        <a class="nav-link" href="barang-masuk-user.php">
+                            <div class="sb-nav-link-icon"><i class="fa-sharp fa-solid fa-warehouse fa-bounce"></i></div>
+                            Bahan Masuk
+                        </a>
+
+                        <a class="nav-link" href="barang-keluar-user.php">
+                            <div class="sb-nav-link-icon"></div>
+                            Bahan Keluar
+                        </a>
+
+
+                    </div>
+                    <div class="nav">
+                        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                        <div class="card-header">
+                            <!-- Button to Open the Modal -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                Ubah Data Akun
+                            </button>
+                        </div>
+                        <a class="nav-link" href="logout.php">
+                            <div class="sb-nav-link-icon"></i></div>
+                            Logout
+                        </a>
+
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4">BAHAN MASUK</h1>
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Kode Barang</th>
+                                            <th>Nama Barang</th>
+                                            <th>Jumlah</th>
+                                            <th>Tanggal</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+
+                                        <?php
+                                        $ambilsemuadatastock = mysqli_query($conn, "select * from masuk");
+                                        while ($data = mysqli_fetch_array($ambilsemuadatastock)) {
+                                            $kodebarang = $data['idbarang'];
+                                            $namabarang = $data['namabarang'];
+                                            $qty = $data['qty'];
+                                            $tanggal = $data['tanggal'];
+                                        ?>
+
+                                            <tr>
+                                                <td><?= $kodebarang; ?></td>
+                                                <td><?= $namabarang; ?></td>
+                                                <td><?= $qty; ?></td>
+                                                <td><?= $tanggal; ?></td>
+                                                
+                                            </tr>
+
+                                            
+                                        <?php
+                                        };
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/datatables-demo.js"></script>
+</body>
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah Bahan</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <form method="post">
+                <div class="modal-body">
+                    <input type="text" name="idbarang" placeholder="ID Barang" class="form-control" required>
+                    <br>
+                    <input type="text" name="namabarang" placeholder="Nama Barang" class="form-control" required>
+                    <br>
+                    <input type="number" name="stock" placeholder="Stock" class="form-control" required>
+                    <br>
+                    <button type="submit" class="btn btn-primary" name="tambah-bahan-masuk">Submit</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+</html>
