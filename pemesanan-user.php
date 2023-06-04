38a208fa5728af52e0565661ce6f427fa0e51ee9
anharsaja <anharmuk12@gmail.com>
2023-06-04 17:13:09 +0700

    fixing title

--- pemesanan-user.php
+++ pemesanan-user.php
@@ -13,7 +13,7 @@ require 'function.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Barang Keluar</title>
+    <title>Pemesanan</title>
     <link rel="icon" type="png" href="assets/img/trade.png">
     <link href="css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
