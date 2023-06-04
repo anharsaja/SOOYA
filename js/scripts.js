c9dddd04803768fd810a1d8a9c4c39c41abc7ce7
anharsaja <anharmuk12@gmail.com>
2023-06-04 17:10:30 +0700

    JS fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-    * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
+
     (function($) {
     "use strict";
 
